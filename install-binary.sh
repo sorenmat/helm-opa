25444e82485473bee2032d3e900b173cc0b7fa1d
Søren Mathiasen <sorenm@mymessages.dk>
2021-12-10 14:15:27 +0100

    Update install-binary.sh

--- install-binary.sh
+++ install-binary.sh
@@ -92,7 +92,7 @@ installFile() {
   HELM_TMP="/tmp/$PROJECT_NAME"
   mkdir -p "$HELM_TMP"
   tar xf "$PLUGIN_TMP_FILE" -C "$HELM_TMP"
-  HELM_TMP_BIN="$HELM_TMP/tpl"
+  HELM_TMP_BIN="$HELM_TMP/opa"
   echo "Preparing to install into ${HELM_PLUGIN_PATH}"
   # Use * to also copy the file withe the exe suffix on Windows
   cp "$HELM_TMP_BIN"* "$HELM_PLUGIN_PATH"
